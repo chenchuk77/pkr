771760a02b6870671188fe3239c2bf16a2b922c2
chenchuk77 <chenchuk@gmail.com>
2019-03-26 16:56:54 +0200

    1. add terminator config file 2. change console clients startup scripts to open 4 ordered windows

--- client/start.sh
+++ client/start.sh
@@ -3,13 +3,28 @@
 PROJECT_FOLDER="${HOME}/dev/pkr"
 ARGS="-fa 'Monospace' -fs 14"
 
-terminator -e "${PROJECT_FOLDER}/client/run-ddd.sh" -T 'ddd' &
+terminator --config ${PROJECT_FOLDER}/client/terminator-config \
+           --profile pokergame \
+           --layout ddd \
+           -e "${PROJECT_FOLDER}/client/run-ddd.sh" -T 'ddd' &
 sleep 2s
-terminator -e "${PROJECT_FOLDER}/client/run-eee.sh" -T 'eee' &
+
+terminator --config ${PROJECT_FOLDER}/client/terminator-config \
+           --profile pokergame \
+           --layout eee \
+           -e "${PROJECT_FOLDER}/client/run-eee.sh" -T 'eee' &
 sleep 2s
-terminator -e "${PROJECT_FOLDER}/client/run-fff.sh" -T 'fff' &
+
+terminator --config ${PROJECT_FOLDER}/client/terminator-config \
+           --profile pokergame \
+           --layout fff \
+           -e "${PROJECT_FOLDER}/client/run-fff.sh" -T 'fff' &
 sleep 2s
-terminator -e "${PROJECT_FOLDER}/client/run-iii.sh" -T 'iii' &
+
+terminator --config ${PROJECT_FOLDER}/client/terminator-config \
+           --profile pokergame \
+           --layout iii \
+           -e "${PROJECT_FOLDER}/client/run-iii.sh" -T 'iii' &
 
 echo "sleep forever to keep clients up"
 while true; do
